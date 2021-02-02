9df62e7447473e1b77cbbe013b11c43d0dd85243
Pradeep <pradeepkumarraq@gmail.com>
2021-02-02 12:27:29 +0530

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector(".menu-btn");
+const hamburger = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector("nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav__item");
+
+menuBtn.addEventListener("click", () => {
+  hamburger.classList.toggle("open");
+  nav.classList.toggle("open");
+  menuNav.classList.toggle("open");
+  navItems.forEach((item) => {
+    item.classList.toggle("open");
+  });
+  console.log("hello");
+});
